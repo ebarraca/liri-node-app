cdb93121cc014a713ba6f8db8524338435848dbe
Erica Barraca <ericabarraca@MacBook-Pro.local>
2018-02-09 16:43:35 -0600

    friday second commit

--- liri.js
+++ liri.js
@@ -31,12 +31,6 @@ var omdb = require('omdb');
 // // 	nicetohaves: nicetohaves
 
 
-
-
-
-//3 if statements if process.argv[2] === "my tweet"
-//then....
-
 if (process.argv[2] === "my-tweets") {
 
     var client = new Twitter({
@@ -127,5 +121,22 @@ if (process.argv[2] === "movie-this") {
           console.log(OMDBResults);
 
       };
+    });
+};
+if (process.argv[2] === "do-what-it-says") {
+    var fs = require("fs");
+    fs.readFile("random.txt", "utf8", function(error, data) {
+
+      // If the code experiences any errors it will log the error to the console.
+      if (error) {
+        return console.log(error);
+      }
+
+      // We will then print the contents of data
+      console.log(data);
+
+      // Then split it by commas (to make it more readable)
+      var dataArr = data.split(",");
+
     });
 };
