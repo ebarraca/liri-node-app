d3b0fe1f1b2df971a9c7063dd1832415f45ac4a6
Erica Barraca <ericabarraca@MacBook-Pro.local>
2018-02-10 21:50:45 -0600

    final commit

--- liri.js
+++ liri.js
@@ -1,35 +1,9 @@
-//
-// 8. At the top of the `liri.js` file, add code to read and set any environment variables with the dotenv package:
 require('dotenv').config()
-
 var Spotify = require('node-spotify-api');
-
 var Twitter = require('twitter');
-
 var request = require('request');
-
 var omdb = require('omdb');
 var keys = require('./key.js')
-
-// ```js
-// require("dotenv").config();
-// ```
-//
-// 9. Add the code required to import the `keys.js` file and store it in a variable.
-//
-// * You should then be able to access your keys information like so
-//
-//   ```js
-//   var spotify = new Spotify(keys.spotify);
-//   var client = new Twitter(keys.twitter);
-//
-// var key = require('./key.js');
-// var spotify = new Spotify(key.spotify);
-// var client = new Twitter(key.twitter);
-
-// // module.exports = {
-// // 	essentials: essentials,
-// // 	nicetohaves: nicetohaves
 
 
 if (process.argv[2] === "my-tweets") {
@@ -49,19 +23,15 @@ if (process.argv[2] === "my-tweets") {
                   "-------------------------------------------------------------" + "\r\n";
                   console.log(twitterResults);
 
-      };
+           };
 
       }
-    });
-}
+     });
+   }
 
-if (process.argv[2] === "spotify-this-song") {
-    var songTitle = process.argv[3];
-
-    //this is b/c user types "spotify this song, and then the song name"
-
-var spotify = new Spotify(keys.spotify);
-
+   if (process.argv[2] === "spotify-this-song") {
+    var songTitle = process.argv[3] ? process.argv[3] : "'The Sign' by Ace of Base";
+    var spotify = new Spotify(keys.spotify);
 
     spotify.search({ type: 'track', query: songTitle }, function(err, data) {
         if ( err ) {
@@ -79,58 +49,44 @@ var spotify = new Spotify(keys.spotify);
                     "Preview Url: " + songInfo[i].preview_url + "\r\n" +
                     "-------------------------------------------------------------" + "\r\n";
                     console.log(spotifyResults);
+
             }
-    });
-}
+        });
+    }
 
-// * If no song is provided then your program will default to "The Sign" by Ace of Base.
 
-//
-if (process.argv[2] === "movie-this") {
-    var movie = process.argv[3];
-    // Then run a request to the OMDB API with the movie specified
-    var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+    if (process.argv[2] === "movie-this") {
+        var movie = process.argv[3] ? process.argv[3] : "Mr. Nobody";
+        var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+        var request = require("request");
+        request(queryUrl, function(error, response, body) {
 
-    // This line is just to help us debug against the actual URL.
-    // console.log(queryUrl);
+          if (!error && response.statusCode === 200) {
 
-    // Then create a request to the queryUrl
-    var request = require("request");
-    request(queryUrl, function(error, response, body) {
+              var OMDBResults =
+                  "Year: " + JSON.parse(body).Year + "\r\n" +
+                  "IMDB Rating: " + JSON.parse(body).imdbRating + "\r\n" +
+                  "Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value + "\r\n" +
+                  "Country: " + JSON.parse(body).Country + "\r\n" +
+                  "Language: " + JSON.parse(body).Language + "\r\n" +
+                  "Plot: " + JSON.parse(body).Plot + "\r\n" +
+                  "Actors: " + JSON.parse(body).Actors + "\r\n" +
+                  "-------------------------------------------------------------" + "\r\n";
+                  console.log(OMDBResults);
 
-      // If the request is successful
-      if (!error && response.statusCode === 200) {
-      // ...
-
-      var OMDBResults =
-          "Year: " + JSON.parse(body).Year + "\r\n" +
-          "IMDB Rating: " + JSON.parse(body).imdbRating + "\r\n" +
-          "Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value + "\r\n" +
-          "Country: " + JSON.parse(body).Country + "\r\n" +
-          "Language: " + JSON.parse(body).Language + "\r\n" +
-          "Plot: " + JSON.parse(body).Plot + "\r\n" +
-          "Actors: " + JSON.parse(body).Actors + "\r\n" +
-          "-------------------------------------------------------------" + "\r\n";
-          console.log(OMDBResults);
-          // console.log(JSON.parse(body));
-
-      };
-    });
+         };
+        });
 };
-if (process.argv[2] === "do-what-it-says") {
-    var fs = require("fs");
-    fs.readFile("random.txt", "utf8", function(error, data) {
+    if (process.argv[2] === "do-what-it-says") {
+        var fs = require("fs");
+        fs.readFile("random.txt", "utf8", function(error, data) {
 
-      // If the code experiences any errors it will log the error to the console.
-      if (error) {
-        return console.log(error);
-      }
+          if (error) {
+            return console.log(error);
+          }
+            console.log(data);
 
-      // We will then print the contents of data
-      console.log(data);
+            var dataArr = data.split(",");
 
-      // Then split it by commas (to make it more readable)
-      var dataArr = data.split(",");
-
-    });
-};
+        });
+    };
